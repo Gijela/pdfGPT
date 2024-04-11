f95fcf1254d0e2631d289e0958b262b9a66013d3
Gijela <961323961@qq.com>
2024-04-11 21:35:34 +0800

    feat: use local apikey

--- src/pages/api/createMessage.ts
+++ src/pages/api/createMessage.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function createMessage(req: NextApiRequest, res: NextApiResponse) {
-  const { messages } = req.body
-  const apiKey = process.env.OPENAI_API_KEY
+  const { messages, apiKey } = req.body
+  // const apiKey = process.env.OPENAI_API_KEY
   const url = `${process.env.BASE_URL}/v1/chat/completions`
 
   const body = JSON.stringify({
--- src/utils/sendMessage.ts
+++ src/utils/sendMessage.ts
@@ -2,12 +2,14 @@ import { ChatCompletionRequestMessage } from 'openai'
 
 export const sendMessage = async (messages: ChatCompletionRequestMessage[]) => {
   try {
+    const apiKey = localStorage.getItem('chat2hub_pdf_ApiKey') || ''
+
     const response = await fetch('/api/createMessage', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ messages })
+      body: JSON.stringify({ messages, apiKey })
     })
 
     return await response.json()
